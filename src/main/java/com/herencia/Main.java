491de52f4ccbbde5e329ae14521a8849d3a86a0b
Miguel Londoño <magnolinneo1971@gmail.com>
2026-03-09 17:58:10 -0500

    Se termino este trabajo de herencia en java

--- src/main/java/com/herencia/Main.java
+++ src/main/java/com/herencia/Main.java
@@ -10,10 +10,14 @@ public class Main {
         cuenta1.imprimir();
 
         cuentaAhorros cuenta2 = new cuentaAhorros(15000, 0.105f);
-        cuenta2.ejecutarOperacion(); // Debería ejecutar la operación
+        cuenta2.ejecutarOperacion();
         cuenta2.imprimir2();
-        
-            
+
+        cuentaCorriente cuenta3 = new cuentaCorriente(2000, 0.1f);
+        cuenta3.retirar(1000); // Debería permitir el retiro y mostrar el sobregiro;
+        cuenta3.consignar(500); // Debería reducir el sobregiro
+        cuenta3.extracto_mensual();
+        cuenta3.imprimir3();
 
     }
 
